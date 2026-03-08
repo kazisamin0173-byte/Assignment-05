e0cf01a94b16c1569de9dd6a83fa86b23bbee3ac
kazisamin0173-byte <kazisamin0173@gmail.com>
2026-03-09 02:05:20 +0600

    sign in to home page

---
+++ script/sign.js
@@ -0,0 +1,16 @@
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", function () {
+  const userInput = document.getElementById("userInput");
+  const inputValue = userInput.value;
+
+  const userPin = document.getElementById("userPin");
+  const pin = userPin.value;
+  if (inputValue == "admin" && pin == "admin123") {
+    
+    window.location.assign("./home.html")
+    alert("Sign in Successful");
+  } else {
+    alert("please input right username and password");
+  }
+});
